5c23115f1e6343a81d36f7d14cdd73bb6000e048
Denis Boisset <denis.boisset@orange.com>
2016-07-08 10:41:53 +0200

    Refactoring

--- app/src/main/java/com/orange/spring/demo/biz/view/model/SignProfileView.java
+++ app/src/main/java/com/orange/spring/demo/biz/view/model/SignProfileView.java
@@ -30,6 +30,7 @@ import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -44,12 +45,9 @@ public class SignProfileView {
     associateIds.addAll(sign.refrenceBySignsIds);
     this.associateSignsIds = associateIds;
 
-    List<Sign> listSignWithOutId = new ArrayList<>();
-    for (Sign signe:signService.all().list()) {
-      if (signe.id != sign.id) {
-        listSignWithOutId.add(signe);
-      }
-    }
+    List<Sign> listSignWithOutId = signService.all().list()
+            .stream().filter(signe -> signe.id != sign.id)
+            .collect(Collectors.toList());
     this.allSignsWithoutCurrentSign = listSignWithOutId;
 
   }
