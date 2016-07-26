136af7681dd8200befd92028e657348490ca21a4
Denis Boisset <denis.boisset@orange.com>
2016-07-26 11:18:29 +0200

    Adding Suggest View

--- app/src/main/java/com/orange/signsatwork/biz/view/controller/SuggestController.java
+++ app/src/main/java/com/orange/signsatwork/biz/view/controller/SuggestController.java
@@ -23,32 +23,31 @@ package com.orange.signsatwork.biz.view.controller;
  */
 
 import com.orange.signsatwork.biz.domain.Request;
-import com.orange.signsatwork.biz.domain.Sign;
 import com.orange.signsatwork.biz.domain.User;
 import com.orange.signsatwork.biz.persistence.service.MessageByLocaleService;
 import com.orange.signsatwork.biz.persistence.service.Services;
-import com.orange.signsatwork.biz.view.model.*;
+import com.orange.signsatwork.biz.view.model.AuthentModel;
+import com.orange.signsatwork.biz.view.model.RequestCreationView;
+import com.orange.signsatwork.biz.view.model.RequestView;
+import com.orange.signsatwork.biz.view.model.SignCreationView;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
-public class RequestController {
+public class SuggestController {
 
   private static final String HOME_URL = "/";
-  private static final String REQUEST_URL = "/sec/request";
+  private static final String REQUEST_URL = "/sec/suggest";
 
   @Autowired
   private Services services;
@@ -58,36 +57,14 @@ public class RequestController {
 
   @RequestMapping(value = REQUEST_URL)
   public String signs(Principal principal, Model model) {
-    fillModelWithContext(model, "sign.request", principal, HOME_URL);
+    fillModelWithContext(model, "sign.suggest", principal, HOME_URL);
     fillModelWithRequests(model, principal);
     model.addAttribute("showTooltip", true);
-    model.addAttribute("requestCreationView", new RequestCreationView());
+    model.addAttribute("signCreationView", new SignCreationView());
 
-    return "request";
+    return "suggest";
   }
 
-  @Secured("ROLE_USER")
-  @RequestMapping(value = "/sec/request/create", method = RequestMethod.POST)
-  public String createRequest(@ModelAttribute RequestCreationView requestCreationView, Principal principal) {
-    User user = services.user().withUserName(principal.getName());
-    Request request = services.request().create(user.id, requestCreationView.getRequestName());
-
-    log.info("createRequest: username = {} / request name = {}", user.username, requestCreationView.getRequestName());
-
-    return "redirect:/sec/request/";
-  }
-
-  @Secured("ROLE_USER")
-  @RequestMapping(value = "/sec/request/{requestId}/add/sign", method = RequestMethod.POST)
-  public String changeSignRequest(
-          javax.servlet.http.HttpServletRequest req, @PathVariable long requestId, Model model, @ModelAttribute SignCreationView signCreationView, Principal principal) {
-
-    User user = services.user().withUserName(principal.getName());
-    Sign sign = services.sign().create(user.id, signCreationView.getSignName(), signCreationView.getVideoUrl());
-    services.request().changeSignRequest(requestId, sign.id);
-
-    return "redirect:/sign/" + sign.id;
-  }
 
   private void fillModelWithContext(Model model, String messageEntry, Principal principal, String backUrl) {
     model.addAttribute("title", messageByLocaleService.getMessage(messageEntry));
@@ -97,11 +74,6 @@ public class RequestController {
 
   private void fillModelWithRequests(Model model, Principal principal) {
     User user = services.user().withUserName(principal.getName());
-    List<RequestView> myrequestsViewWithoutSignAssociate = RequestView.from(services.request().requestsforUserWithoutSignAssociate(user.id));
-    model.addAttribute("myRequestsWithoutSignAssociate", myrequestsViewWithoutSignAssociate);
-
-    List<RequestView> myrequestsViewWithSignAssociate = RequestView.from(services.request().requestsforUserWithSignAssociate(user.id));
-    model.addAttribute("myRequestsWithSignAssociate", myrequestsViewWithSignAssociate);
 
     List<RequestView> otherrequestsViewWithoutSignAssociate = RequestView.from(services.request().requestsforOtherUserWithoutSignAssociate(user.id));
     model.addAttribute("otherRequestsWithoutSignAssociate", otherrequestsViewWithoutSignAssociate);
