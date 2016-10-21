f87630ba9d037e99c20ba5895cfcc02a91e09091
Denis Boisset <denis.boisset@orange.com>
2016-10-21 10:32:41 +0200

    Video dailymotion put in iframe to play with dailymotion player

--- app/src/main/java/com/orange/signsatwork/biz/view/model/SignProfileView.java
+++ app/src/main/java/com/orange/signsatwork/biz/view/model/SignProfileView.java
@@ -10,12 +10,12 @@ package com.orange.signsatwork.biz.view.model;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -55,16 +55,18 @@ public class SignProfileView {
 
   public SignProfileView(Sign sign, SignService signService, User user) {
     this.sign = sign;
+    this.url = sign.url;
 
-    if (sign.url.contains("www.dailymotion.com/embed/video")) {
-      if (signService.getStreamUrl(sign.url) != null) {
-        this.url = signService.getStreamUrl(sign.url);
-      } else {
-        this.url = sign.url;
-      }
-    } else {
-      this.url = sign.url;
-    }
+//    This part allowed getStreamUrl without this code we have embed video and video put in iframe in the html code
+//    if (sign.url.contains("www.dailymotion.com/embed/video")) {
+//      if (signService.getStreamUrl(sign.url) != null) {
+//        this.url = signService.getStreamUrl(sign.url);
+//      } else {
+//        this.url = sign.url;
+//      }
+//    } else {
+//      this.url = sign.url;
+//    }
     List<Long> associateIds = sign.associateSignsIds;
     associateIds.addAll(sign.referenceBySignsIds);
     this.associateSignsIds = associateIds;
