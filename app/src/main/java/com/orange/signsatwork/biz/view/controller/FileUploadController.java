d66327059848e76a41db19f2b9ce7cb6be72ca5b
Denis Boisset <denis.boisset@orange.com>
2017-01-03 14:55:28 +0100

    Revert from deleted file

---
+++ app/src/main/java/com/orange/signsatwork/biz/view/controller/FileUploadController.java
@@ -0,0 +1,67 @@
+package com.orange.signsatwork.biz.view.controller;
+
+/*
+ * #%L
+ * Signs at work
+ * %%
+ * Copyright (C) 2016 Orange
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+import com.orange.signsatwork.biz.storage.StorageFileNotFoundException;
+import com.orange.signsatwork.biz.storage.StorageService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class FileUploadController {
+
+
+  @Autowired
+  private StorageService storageService;
+
+
+  @GetMapping("/files/{filename:.+}")
+  @ResponseBody
+  public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+    Resource file = storageService.loadAsResource(filename);
+    return ResponseEntity
+      .ok()
+      .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename()+"\"")
+      .body(file);
+  }
+
+
+  @ExceptionHandler(StorageFileNotFoundException.class)
+  public ResponseEntity handleStorageFileNotFound(StorageFileNotFoundException exc) {
+    return ResponseEntity.notFound().build();
+  }
+
+
+}
+
+
