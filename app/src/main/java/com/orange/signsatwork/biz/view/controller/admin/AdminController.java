c47b9ab17c69cfddd496b856c77f4e7f3b608298
Denis Boisset <denis.boisset@orange.com>
2017-01-13 08:06:01 +0100

    Delete testUI interface

--- app/src/main/java/com/orange/signsatwork/biz/view/controller/admin/AdminController.java
+++ app/src/main/java/com/orange/signsatwork/biz/view/controller/admin/AdminController.java
@@ -10,12 +10,12 @@ package com.orange.signsatwork.biz.view.controller.admin;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -28,11 +28,11 @@ import com.orange.signsatwork.biz.persistence.service.CommunityService;
 import com.orange.signsatwork.biz.persistence.service.MessageByLocaleService;
 import com.orange.signsatwork.biz.persistence.service.Services;
 import com.orange.signsatwork.biz.persistence.service.UserService;
-import com.orange.signsatwork.biz.view.controller.testUI.UserTestUIController;
 import com.orange.signsatwork.biz.view.model.AuthentModel;
 import com.orange.signsatwork.biz.view.model.CommunityView;
 import com.orange.signsatwork.biz.view.model.UserCreationView;
 import com.orange.signsatwork.biz.view.model.UserView;
+import org.jcodec.api.JCodecException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
@@ -42,11 +42,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class AdminController {
 
+//  @Autowired
+//  private UserTestUIController userTestUIController;
+
   @Autowired
-  private UserTestUIController userTestUIController;
+  private UserAdminController userAdminController;
 
   @Autowired
   private Services services;
@@ -63,7 +73,7 @@ public class AdminController {
   public String admin(Model model) {
 
     AuthentModel.addAuthenticatedModel(model, true);
-    model.addAttribute("title", messageByLocaleService.getMessage("testUI_page"));
+    model.addAttribute("title", messageByLocaleService.getMessage("admin_page"));
 
     return "admin/index";
   }
@@ -107,12 +117,6 @@ public class AdminController {
     return "admin/community";
   }
 
-  @Secured("ROLE_ADMIN")
-  @RequestMapping(value = "/sec/admin/user/create", method = RequestMethod.POST)
-  public String user(@ModelAttribute UserCreationView userCreationView, Model model) {
-    User user = userService.create(userCreationView.toUser(), userCreationView.getPassword());
-    return userTestUIController.userDetails(user.id, model);
-  }
 
   @Secured("ROLE_ADMIN")
   @RequestMapping(value = "/sec/admin/community/create", method = RequestMethod.POST)
@@ -121,5 +125,49 @@ public class AdminController {
     return community(community.id, model);
   }
 
+  @Secured("ROLE_ADMIN")
+  @RequestMapping(value = "/sec/admin/user/create", method = RequestMethod.POST)
+  public String user(@ModelAttribute UserCreationView userCreationView, Model model) throws IOException, JCodecException {
 
+    User user = userService.create(userCreationView.toUser(), userCreationView.getPassword());
+    userService.createUserFavorite(user.id, messageByLocaleService.getMessage("default_favorite"));
+    return userAdminController.userDetails(user.id, model);
+  }
+
+
+  @Secured("ROLE_ADMIN")
+  @RequestMapping(value = "/sec/admin/user/{userId}/add/communities", method = RequestMethod.POST)
+  /**
+   * We retrieve all form parameters directly from the raw request since in this case
+   * we can not rely on a json object deserialization.
+   * Indeed, POST form parameters look like this:
+   *  - userCommunitiesIds -> "12"
+   *  - userCommunitiesIds -> "34"
+   *  - ...
+   *  which in this case means that the user belongs to communities with id 12 & 34
+   *
+   *  Then we resend the user details page
+   */
+  public String changeUserCommunities(
+    HttpServletRequest req, @PathVariable long userId, Model model) {
+
+    List<Long> communitiesIds =
+      transformCommunitiesIdsToLong(req.getParameterMap().get("userCommunitiesIds"));
+
+    userService.changeUserCommunities(userId, communitiesIds);
+
+    return userAdminController.userDetails(userId, model);
+  }
+
+
+
+  /** The form POST provides Ids as String, we convert it back to Long */
+  private List<Long> transformCommunitiesIdsToLong(String[] userCommunitiesIds) {
+    if (userCommunitiesIds == null) {
+      return new ArrayList<>();
+    }
+    return Arrays.asList(userCommunitiesIds).stream()
+      .map(Long::parseLong)
+      .collect(Collectors.toList());
+  }
 }
