49bbd02cf9f9befc68eccb34eccbb94f4e814219
SANCHEZ Manuel <manuel1.sanchez@orange.com>
2017-01-17 11:32:33 +0100

    kanbantask #357941 ask sign : fit

---
+++ app/src/main/resources/public/js/request-detail.js
@@ -0,0 +1,56 @@
+/*
+ * #%L
+ * Telsigne
+ * %%
+ * Copyright (C) 2016 Orange
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+console.log("Cool, request-detail.js is loaded :)");
+
+
+function onPrioriseRequest(id){
+  $.ajax({
+    url: "/ws/sec/request/"+ id +"/priorise",
+    type: 'post',
+    success: function(response) {
+
+      $("#priorise_request").modal('show');
+    },
+    error: function(response) {
+      }
+  })
+
+};
+
+function onDeleteRequest(id){
+  $.ajax({
+    url: "/ws/sec/request/"+ id +"/delete",
+    type: 'post',
+    success: function(response) {
+
+      $("#delete_request").modal('show');
+    },
+    error: function(response) {
+      }
+  })
+
+};
+
+function onContinue() {
+  var url = "/sec/request/";
+  window.location = url;
+};
+
