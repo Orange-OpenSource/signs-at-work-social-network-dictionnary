9db9106bd23dc2ab34aa257517a82461ac204540
Denis Boisset <denis.boisset@orange.com>
2017-03-02 12:00:31 +0100

    Add Favorite Item Menu in index page

--- app/src/main/java/com/orange/signsatwork/biz/view/controller/HomeController.java
+++ app/src/main/java/com/orange/signsatwork/biz/view/controller/HomeController.java
@@ -10,12 +10,12 @@ package com.orange.signsatwork.biz.view.controller;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -132,10 +132,10 @@ public class HomeController {
     model.addAttribute("nbRecentSign", createdSinceLastDeconnection.size());
 //    model.addAttribute("signsView", signViews);
 //    model.addAttribute("signCreationView", new SignCreationView());
-//    if (AuthentModel.isAuthenticated(principal)) {
-//      fillModelWithFavorites(model, user);
-//    }
-//    model.addAttribute("favoriteCreationView", new FavoriteCreationView());
+    if (AuthentModel.isAuthenticated(principal)) {
+      fillModelWithFavorites(model, user);
+    }
+    model.addAttribute("favoriteCreationView", new FavoriteCreationView());
 //    model.addAttribute("signSearchView", new SignSearchView());
     model.addAttribute("isDevProfile", appProfile.isDevProfile());
     model.addAttribute("isAlphabeticAsc", true);
