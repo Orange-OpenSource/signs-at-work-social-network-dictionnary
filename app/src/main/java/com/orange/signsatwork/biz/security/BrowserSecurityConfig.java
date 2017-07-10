3f17fc29a9a03099c5879538437886ebbde268bc
Denis Boisset <denis.boisset@orange.com>
2017-07-10 13:24:20 +0200

    Can't use application when are not connected

--- app/src/main/java/com/orange/signsatwork/biz/security/BrowserSecurityConfig.java
+++ app/src/main/java/com/orange/signsatwork/biz/security/BrowserSecurityConfig.java
@@ -10,12 +10,12 @@ package com.orange.signsatwork.biz.security;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -56,7 +56,7 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
             // configure restricting access
             .authorizeRequests()
             // open api is... opened
-            .antMatchers("/", "/signs/**", "/sign/**").permitAll()
+//            .antMatchers("/", "/signs/**", "/sign/**").permitAll()
             // admin api restricted to... ADMIN
             .antMatchers("/sec/admin/**").hasRole("ADMIN")
             // and the rest is allowed by any authenticated user
--- app/src/main/java/com/orange/signsatwork/biz/view/controller/HomeController.java
+++ app/src/main/java/com/orange/signsatwork/biz/view/controller/HomeController.java
@@ -10,12 +10,12 @@ package com.orange.signsatwork.biz.view.controller;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -60,7 +60,13 @@ public class HomeController {
   @RequestMapping("/")
   public String index(HttpServletRequest req, Principal principal, Model model) {
     long t0 = System.currentTimeMillis();
-    String pageName = doIndex(req, principal, model);
+    String pageName;
+    if (AuthentModel.isAuthenticated(principal)) {
+      pageName = doIndex(req, principal, model);
+    } else {
+      pageName = "login";
+    }
+
     long dt = System.currentTimeMillis() - t0;
     log.info("[PERF] took " + dt + " ms to process root page request");
     return pageName;
