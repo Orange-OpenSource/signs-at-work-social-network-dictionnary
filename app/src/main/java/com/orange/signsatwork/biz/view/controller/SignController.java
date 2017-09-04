1ec45e6c11a064b935c5aec5652b7bbe08f73978
SANCHEZ Manuel <manuel1.sanchez@orange.com>
2017-09-04 09:45:24 +0200

    kanban#386213  picto share

--- app/src/main/java/com/orange/signsatwork/biz/view/controller/SignController.java
+++ app/src/main/java/com/orange/signsatwork/biz/view/controller/SignController.java
@@ -10,12 +10,12 @@ package com.orange.signsatwork.biz.view.controller;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -520,7 +520,7 @@ public class SignController {
     String referer = req.getHeader("Referer");
     String backUrl;
     model.addAttribute("videoBelowToFavorite", false);
-
+    StringBuffer location = req.getRequestURL();
 
     AuthentModel.addAuthenticatedModel(model, AuthentModel.isAuthenticated(principal));
     model.addAttribute("showAddFavorite", SHOW_ADD_FAVORITE && AuthentModel.isAuthenticated(principal));
@@ -550,6 +550,9 @@ public class SignController {
     Video video = services.video().withId(videoId);
     if (principal != null) {
       User user = services.user().withUserName(principal.getName());
+      if(user != null) {
+        model.addAttribute("mail_body", messageByLocaleService.getMessage("share_application_body", new Object[]{user.name(), location}));
+      }
       Object[] queryRating = services.video().RatingForVideoByUser(videoId, user.id);
       RatingData ratingData = new RatingData(queryRating);
       model.addAttribute("ratingData", ratingData);
