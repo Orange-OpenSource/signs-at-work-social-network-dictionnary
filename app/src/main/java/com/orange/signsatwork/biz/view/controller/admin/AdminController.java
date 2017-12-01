ab78f6509a7f36f11f9dca135ea3af619a17c907
denis <denis.boisset@orange.com>
2017-12-01 13:53:07 +0100

    feature add change password in admin interface

--- app/src/main/java/com/orange/signsatwork/biz/view/controller/admin/AdminController.java
+++ app/src/main/java/com/orange/signsatwork/biz/view/controller/admin/AdminController.java
@@ -10,12 +10,12 @@ package com.orange.signsatwork.biz.view.controller.admin;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -156,6 +156,14 @@ public class AdminController {
     return userAdminController.userDetails(userId, model);
   }
 
+  @Secured("ROLE_ADMIN")
+  @RequestMapping(value = "/sec/admin/user/{userId}/changePassword", method = RequestMethod.POST)
+  public String changeUserPassword(@ModelAttribute UserCreationView userCreationView, @PathVariable long userId, Model model) {
+
+   userService.changeUserPassword(userService.withId(userId), userCreationView.getPassword());
+
+    return userAdminController.userDetails(userId, model);
+  }
 
 
   /** The form POST provides Ids as String, we convert it back to Long */
