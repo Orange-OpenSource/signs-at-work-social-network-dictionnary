e424823fe7911cb9216757ea6c583c2d9f89ebf4
denis <denis.boisset@orange.com>
2018-05-23 10:20:55 +0200

    feature new design

--- app/src/main/java/com/orange/signsatwork/biz/view/controller/RequestController.java
+++ app/src/main/java/com/orange/signsatwork/biz/view/controller/RequestController.java
@@ -378,7 +378,7 @@ public class RequestController {
     model.addAttribute("isMostRecent", false);
     model.addAttribute("isLowRecent", false);
     model.addAttribute("dropdownTitle", messageByLocaleService.getMessage("allRequests"));
-    model.addAttribute("classDropdownTitle", " signe pull-left");
+    model.addAttribute("classDropdownTitle", " all_signe pull-left");
     model.addAttribute("classDropdownSize", "adjust_size btn btn-default dropdown-toggle");
     model.addAttribute("isSearch", isSearch);
 
