dba7ee9ebf3389648fb556dd3d1d567a36679e90
denis <denis.boisset@orange.com>
2019-09-24 14:59:46 +0200

    fix

--- app/src/main/resources/migrate.sql
+++ app/src/main/resources/migrate.sql
@@ -3,3 +3,4 @@ update favorites set favorites.name = "Ma liste", favorites.type="Individual" wh
 insert into communities_users select communities_id, users_id from users_communities;
 drop table users_communities;
 update favorites set id_for_name=0;
+delete from ratings where rating='Neutral';
