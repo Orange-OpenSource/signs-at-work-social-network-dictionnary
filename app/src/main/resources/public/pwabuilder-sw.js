e205275a98a60a42a19d39bda1eac281ebe44bd5
denis <denis.boisset@orange.com>
2019-12-10 09:57:27 +0100

    feature pwa for signsatform

--- app/src/main/resources/public/pwabuilder-sw.js
+++ app/src/main/resources/public/pwabuilder-sw.js
@@ -13,10 +13,6 @@ self.addEventListener("install", function (event) {
     caches.open(CACHE).then(function (cache) {
       console.log("[PWA Builder] Cached offline page during install");
 
-      if (offlineFallbackPage === "offline.html") {
-        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
-      }
-
       return cache.add(offlineFallbackPage);
     })
   );
