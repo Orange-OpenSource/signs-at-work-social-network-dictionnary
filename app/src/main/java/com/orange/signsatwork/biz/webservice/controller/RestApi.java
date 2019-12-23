22ff0e3b146aa9178fc82409c5d50e1d41c14661
denis <denis.boisset@orange.com>
2019-12-23 14:18:35 +0100

    Feature profil page

--- app/src/main/java/com/orange/signsatwork/biz/webservice/controller/RestApi.java
+++ app/src/main/java/com/orange/signsatwork/biz/webservice/controller/RestApi.java
@@ -66,6 +66,7 @@ public class RestApi {
   public static final String FORGET_PASSWORD = "/forgetPassword";
   public static final String SAVE_PASSWORD = "/user/{userId}/savePassword";
   public static final String SEND_MAIL = "/sendMail";
+  public static final String SEND_MAIL_FOR_CHANGE_EMAIL = WS_SEC + "/sendMailForChangeEmail";
   /** Fin API REST For Android and IOS **/
 
   public static final String WS_OPEN_SIGN = WS_OPEN + "sign/";
