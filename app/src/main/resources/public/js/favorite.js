0e6417c470b7234927903741ed2245c709c374c0
denis <denis.boisset@orange.com>
2020-01-30 11:20:02 +0100

    Fix back navigation

--- app/src/main/resources/public/js/favorite.js
+++ app/src/main/resources/public/js/favorite.js
@@ -76,7 +76,12 @@ function onAssociateFavoriteCommunities(favoriteId, communityId) {
         $("#validate_share_favorite_modif").modal('show');
         setTimeout(function () {
           $('#validate_share_favorite_modif').modal('hide');
-          window.history.back();
+          url = "/sec/favorite/" +favoriteId;
+          console.log("old "+window.location.href);
+          window.history.replaceState({}, 'foo', url);
+          console.log("new "+window.location.href);
+          window.location = url;
+         /* window.history.back();*/
         }, 3000);
       },
       error: function (response) {
@@ -122,9 +127,16 @@ function onCreateFavoriteCommunity(name, favoriteId) {
           $('#validate_create_community_favorite').modal('hide');
           if (favoriteId == 0) {
             url = "/sec/community/" + communityId;
+            console.log("old "+window.location.href);
+            window.history.replaceState({}, 'foo', url);
+            console.log("new "+window.location.href);
           } else {
             url = "/sec/favorite/share/?id=" + favoriteId + "&communityId=" + communityId;
+            console.log("old "+window.location.href);
+            window.history.replaceState({}, 'foo', "/sec/favorite/"+favoriteId);
+            console.log("new "+window.location.href);
           }
+
           window.location = url;
         }, 3000);
       },
@@ -133,7 +145,6 @@ function onCreateFavoriteCommunity(name, favoriteId) {
     })
   } else {
     window.history.back();
-
   }
 
 };
---
+++ app/src/main/resources/public/js/suggest-communities.js
@@ -0,0 +1,36 @@
+/*
+ * #%L
+ * Telsigne
+ * %%
+ * Copyright (C) 2016 Orange
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+function onChangeBrowserHistory() {
+  console.log("onChangeBrowserHistory");
+  console.log("old "+window.location.href);
+  window.history.replaceState({}, 'foo', "/sec/communities");
+  console.log("new "+window.location.href);
+
+}
+
+function onChangeFavoriteBrowserHistory(favoriteId) {
+  console.log("onChangeFavoriteBrowserHistory");
+  console.log("old "+window.location.href);
+  window.history.replaceState({}, 'foo', "/sec/favorite/"+favoriteId);
+  console.log("new "+window.location.href);
+
+}
