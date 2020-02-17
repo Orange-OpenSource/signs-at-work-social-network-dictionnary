c068d23d82df548beb90cc8381988120b578b61d
denis <denis.boisset@orange.com>
2020-02-17 11:03:00 +0100

    fix branche

--- app/src/main/java/com/orange/signsatwork/biz/view/controller/HomeController.java
+++ app/src/main/java/com/orange/signsatwork/biz/view/controller/HomeController.java
@@ -68,8 +68,12 @@ public class HomeController {
   @Value("${display-url}")
   private String display_url;
 
+
   @Value("${app.name}")
   String appName;
+
+  @Value("${app.manifest}")
+  private String manifest;
 
   @Autowired
   public EmailServiceImpl emailService;
@@ -97,6 +101,7 @@ public class HomeController {
     model.addAttribute("isDevProfile", appProfile.isDevProfile());
     model.addAttribute("signCreationView", new SignCreationView());
     model.addAttribute("display_url", display_url);
+    model.addAttribute("manifest", manifest);
 
 
     return "index";
