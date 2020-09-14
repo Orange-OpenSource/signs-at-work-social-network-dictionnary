402126f74a091bca9b3d527bbb9ffb3df5520d0d
denis <denis.boisset@orange.com>
2020-09-14 16:24:35 +0200

    feature kanbantask #689631

--- app/src/main/java/com/orange/signsatwork/biz/persistence/repository/CommunityRepository.java
+++ app/src/main/java/com/orange/signsatwork/biz/persistence/repository/CommunityRepository.java
@@ -36,7 +36,7 @@ public interface CommunityRepository extends CrudRepository<CommunityDB, Long> {
 
     List<CommunityDB> findByNameStartingWith(String name);
 
-    @Query(value="select id, type, name, user_id, description_text, description_video from communities  where upper(name) collate utf8_unicode_ci like concat(:name,'%')", nativeQuery = true)
+    @Query(value="select id, type, name, user_id, description_text, description_video from communities  where upper(name) collate utf8_unicode_ci like concat('%',:name,'%')", nativeQuery = true)
     List<Object[]> findStartByNameIgnoreCase(@Param("name") String name);
 
     @Query("select distinct c FROM CommunityDB c inner join c.users user where user = :userDB")
