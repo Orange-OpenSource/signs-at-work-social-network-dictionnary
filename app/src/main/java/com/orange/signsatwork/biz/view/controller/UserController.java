9b7c772db995d923c2c883b685a2576aec0c232c
Denis Boisset <denis.boisset@orange.com>
2020-12-24 08:26:16 +0100

    Fix header of page my job

--- app/src/main/java/com/orange/signsatwork/biz/view/controller/UserController.java
+++ app/src/main/java/com/orange/signsatwork/biz/view/controller/UserController.java
@@ -386,6 +386,7 @@ public class UserController {
       .collect(Collectors.toList());
     communitiesViewData = communitiesViewData.stream().sorted((c1, c2) -> c1.name.compareTo(c2.name)).collect(Collectors.toList());
     model.addAttribute("communities", communitiesViewData);
+    model.addAttribute("appName", appName);
 
     return "job-community";
   }
