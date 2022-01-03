194f0ebb2998aeb26fbb5d5127160ff3dbe791c9
Boisset Denis <denis.boisset@orange.com>
2022-01-03 14:31:09 +0100

    Don't activate Message Server for Admin

--- app/src/main/java/com/orange/signsatwork/biz/persistence/service/impl/MessageServerServiceImpl.java
+++ app/src/main/java/com/orange/signsatwork/biz/persistence/service/impl/MessageServerServiceImpl.java
@@ -24,7 +24,7 @@ public class MessageServerServiceImpl implements MessageServerService {
 
   @Override
   public void addMessageServer(MessageServer messageServer) {
-    MessageServerDB messageServerDB = messageServerRepository.save(MessageServerDBFrom(messageServer));
+    /*MessageServerDB messageServerDB = messageServerRepository.save(MessageServerDBFrom(messageServer));*/
   }
 
   @Override
