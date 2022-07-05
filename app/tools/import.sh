9498efc2ab0a8dc809ae465052bacbd6ab0f22d4
Boisset Denis <denis.boisset@orange.com>
2022-07-05 11:23:53 +0200

    Feature adis remark and tools for import in adis

--- app/tools/import.sh
+++ app/tools/import.sh
@@ -24,7 +24,7 @@ function create_sign() {
 	echo "je créé le signe"
 	file="file=@$1"
 	echo $file
-	createSignResponse=$(curl -s --noproxy '*' -u "$3" -H 'Content-Type: multipart/form-data' -X POST -F $file -F 'data={"name":"'"$2"'"};type=application/json' http://localhost:8080/ws/sec/signs)
+	createSignResponse=$(curl -s -u "$3" -H 'Content-Type: multipart/form-data' -X POST -F $file -F 'data={"name":"'"$2"'"};type=application/json' ${BASE_URL}/ws/sec/signs)
 	echo $createSignResponse
 	signId=$(jq -r ".signId"  <<< "${createSignResponse}")
 echo $signId
@@ -36,20 +36,20 @@ echo $errorMessage
 		echo $1 $2 $3 $4 $errorMessage >> $ERRORFILE
 	fi
 	if [ "$4" != null ] && [ "$signId" != null ]; then
-		url=http://localhost:8080/ws/sec/signs/$signId
+		url=${BASE_URL}/ws/sec/signs/$signId
 echo $url
-		createDefinitionSignResponse=$(curl -s --noproxy '*' -u "$3" -H 'Content-Type: multipart/form-data' -X PUT -F 'data={"textDefinition":"'"$4"'"};type=application/json' $url)
+		createDefinitionSignResponse=$(curl -s -u "$3" -H 'Content-Type: multipart/form-data' -X PUT -F 'data={"textDefinition":"'"$4"'"};type=application/json' $url)
 		echo $createDefinitionSignResponse
         fi
-		
+
 }
 
 function create_variante_sign() {
 	echo "je créé une variante du signe"
 	file="file=@$1"
 	echo $file
-	url=http://localhost:8080/ws/sec/signs/$5/videos
-	createVarianteSignResponse=$(curl -s --noproxy '*' -u "$3" -H 'Content-Type: multipart/form-data' -X POST -F $file -F 'data={"name":"'"$2"'"};type=application/json' $url)
+	url=${BASE_URL}/ws/sec/signs/$5/videos
+	createVarianteSignResponse=$(curl -s -u "$3" -H 'Content-Type: multipart/form-data' -X POST -F $file -F 'data={"name":"'"$2"'"};type=application/json' $url)
 	echo $createVarianteSignResponse
 	signId=$(jq -r ".signId"  <<< "${createVarianteSignResponse}")
 echo $signId
@@ -60,15 +60,15 @@ echo $errorMessage
 	if [ "$errorMessage" != null ]; then
 		echo $1 $2 $3 $4 $errorMessage >> $ERRORFILE
 	fi
-		
+
 }
 
 function create_sign_from_request() {
 	echo "je créé un signe en réponse à une demande"
 	file="file=@$1"
 	echo $file
-	url=http://localhost:8080/ws/sec/requests/$4/signs
-	createSignFromRequestResponse=$(curl -s --noproxy '*' -u "$3" -H 'Content-Type: multipart/form-data' -X POST -F $file -F 'data={"name":"'"$2"'"};type=application/json' $url)
+	url=${BASE_URL}/ws/sec/requests/$4/signs
+	createSignFromRequestResponse=$(curl -s -u "$3" -H 'Content-Type: multipart/form-data' -X POST -F $file -F 'data={"name":"'"$2"'"};type=application/json' $url)
 	echo $createSignFromRequestResponse
 	signId=$(jq -r ".signId"  <<< "${createSignFromRequestResponse}")
 echo $signId
@@ -80,18 +80,18 @@ echo $errorMessage
 		echo $1 $2 $3 $4 $errorMessage >> $ERRORFILE
 	fi
 	if [ "$5" != null ] && [ "$signId" != null ]; then
-		url=http://localhost:8080/ws/sec/signs/$signId
+		url=${BASE_URL}/ws/sec/signs/$signId
 echo $url
-		createDefinitionSignResponse=$(curl -s --noproxy '*' -u "$3" -H 'Content-Type: multipart/form-data' -X PUT -F 'data={"textDefinition":"'"$5"'"};type=application/json' $url)
+		createDefinitionSignResponse=$(curl -s -u "$3" -H 'Content-Type: multipart/form-data' -X PUT -F 'data={"textDefinition":"'"$5"'"};type=application/json' $url)
 		echo $createDefinitionSignResponse
         fi
-		
+
 }
 
 function create_favorite() {
 	echo "je créé un favoris"
-	url=http://localhost:8080/ws/sec/favorites
-	createFavorite=$(curl -s --noproxy '*' -u "$2" --header 'Content-Type: application/json' --request POST --data '{"name":"'"$1"'"}' $url)
+	url=${BASE_URL}/ws/sec/favorites
+	createFavorite=$(curl -s -u "$2" --header 'Content-Type: application/json' --request POST --data '{"name":"'"$1"'"}' $url)
 	echo $createFavorite
 	favoriteId=$(jq -r ".favoriteId"  <<< "${createFavorite}")
 echo $favoriteId
@@ -100,7 +100,7 @@ echo $errorMessage
 	if [ "$errorMessage" != null ]; then
 		echo $1 $errorMessage >> $ERRORFILE
 	fi
-		
+
 }
 
 function add_sign_to_favorite() {
@@ -108,19 +108,20 @@ function add_sign_to_favorite() {
 	echo favoriteId $1
 	echo user $2
 	echo videoId $3
-	url=http://localhost:8080/ws/sec/favorites/$1
-	addSignToFavorite=$(curl -s --noproxy '*' -u "$2" --header 'Content-Type: application/json' --request PUT --data '{"videoIdToAdd":"'"$3"'"}' $url)
+	url=${BASE_URL}/ws/sec/favorites/$1
+	addSignToFavorite=$(curl -s -u "$2" --header 'Content-Type: application/json' --request PUT --data '{"videoIdToAdd":"'"$3"'"}' $url)
 	echo $addSignToFavorite
 	errorMessage=$(jq -r ".errorMessage"  <<< "${addSignToFavorite}")
 echo $errorMessage
 	if [ "$errorMessage" != null ]; then
 		echo $1 $errorMessage >> $ERRORFILE
 	fi
-		
+
 }
 
 INPUTFILE=$1
 INPUT_DIRECTORY=$2
+BASE_URL=$3
 ERRORFILE=error.csv
 rm $ERRORFILE
 touch $ERRORFILE
@@ -139,12 +140,12 @@ do
 	fi
 	echo $absoluteFileName
 	if [ ! -f "$absoluteFileName" ]; then
-		echo $line "${absoluteFileName}" "file not exist" >> $ERRORFILE 
+		echo $line "${absoluteFileName}" "file not exist" >> $ERRORFILE
 		continue;
 	fi
 	sign_name="$(cut -d';' -f2 <<<"$line")"
 	echo $sign_name
-	sign_name_encode=$(urlencode "$sign_name") 
+	sign_name_encode=$(urlencode "$sign_name")
 	owner_login="$(cut -d';' -f3 <<<"$line")"
 	echo $owner_login
 	sign_description="$(cut -d';' -f4 <<<"$line")"
@@ -152,7 +153,7 @@ do
 	lists_name="$(cut -d';' -f5 <<<"$line")"
 	echo $lists_name
 	user=$(echo "$owner_login:DEnis0007")
-response=$(curl -s -u ${user} http://localhost:8080/ws/sec/signs?fullname=$sign_name_encode)
+response=$(curl -s -u ${user} ${BASE_URL}/ws/sec/signs?fullname=$sign_name_encode)
 echo RESPONSE : $response
 if [ "$response" != "[]" ]; then
 	name=$(jq -r ".[] | .name"  <<< "${response}")
@@ -165,8 +166,8 @@ if [ "$response" != "[]" ]; then
 		echo "le signe existe déjà"
 		create_variante_sign "${absoluteFileName}" "${sign_name}" $user "$sign_description" $id
 	fi
-else 
-responseSearchRequest=$(curl -s -u ${user} http://localhost:8080/ws/sec/requests?name=$sign_name_encode)
+else
+responseSearchRequest=$(curl -s -u ${user} ${BASE_URL}/ws/sec/requests?name=$sign_name_encode)
 echo $responseSearchRequest
 if [ "$responseSearchRequest" != "[]" ]; then
 	name=$(jq -r ".[] | .name"  <<< "${responseSearchRequest}")
@@ -180,7 +181,7 @@ if [ "$responseSearchRequest" != "[]" ]; then
 				echo "que je n'ai pas créé"
 				create_sign_from_request $absoluteFileName "$sign_name" $user $id "$sign_description"
 			fi
-		
+
 		fi
 	else
 		create_sign "${absoluteFileName}" "${sign_name}" $user "$sign_description"
@@ -189,14 +190,14 @@ if [ "$responseSearchRequest" != "[]" ]; then
 fi
 
 
-while read list_name; 
-do 
+while read list_name;
+do
 
 echo -"$list_name"-
 if [ "${list_name}" ]; then
-	list_name_encode=$(urlencode "$list_name") 
+	list_name_encode=$(urlencode "$list_name")
 echo $list_name_encode
-	url=http://localhost:8080/ws/sec/users/me/favorites?name="$list_name_encode"
+	url=${BASE_URL}/ws/sec/users/me/favorites?name="$list_name_encode"
 echo $url
 	responseSearchFavorite=$(curl -s -u ${user} ${url})
 echo RESPONSESEARCHFAVORITE : $responseSearchFavorite
