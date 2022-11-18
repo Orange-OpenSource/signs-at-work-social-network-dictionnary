17cf32656afbea5ddd64e30492100d8ba36f49b8
Boisset Denis <denis.boisset@orange.com>
2022-11-18 10:32:17 +0100

    Fix API for iOS application and reduce file video size when video are load on server

--- app/src/main/java/com/orange/signsatwork/biz/webservice/controller/VideoRestController.java
+++ app/src/main/java/com/orange/signsatwork/biz/webservice/controller/VideoRestController.java
@@ -2,6 +2,9 @@ package com.orange.signsatwork.biz.webservice.controller;
 
 import com.orange.signsatwork.biz.persistence.service.Services;
 import com.orange.signsatwork.biz.webservice.MultipartFileSender;
+import com.orange.signsatwork.biz.webservice.model.FavoriteCreationViewApi;
+import com.orange.signsatwork.biz.webservice.model.FavoriteResponseApi;
+import com.orange.signsatwork.biz.webservice.model.VideoViewApi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.security.Principal;
 import java.util.List;
 
 
@@ -25,14 +29,14 @@ public class VideoRestController {
 
   @RequestMapping(RestApi.WS_SEC_VIDEOS_ON_SERVER)
   public void getVideo(@PathVariable String name, HttpServletResponse response, HttpServletRequest request) throws Exception {
-    String path = environment.getProperty("app.file")+name;
+    String path = environment.getProperty("app.file") + name;
     MultipartFileSender.fromURIString(path)
       .with(request)
       .with(response)
       .serveResource();
   }
 
-  @Secured("ROLE_USER")
+/*  @Secured("ROLE_USER")
   @RequestMapping(value = RestApi.WS_SEC_VIDEO_FAVORITES_ASSOCIATE, method = RequestMethod.POST)
   public void videoAssociateFavorites(@RequestBody List<Long> videoFavoriteIds, @PathVariable long videoId, HttpServletResponse response) {
 
@@ -40,6 +44,14 @@ public class VideoRestController {
 
     response.setStatus(HttpServletResponse.SC_OK);
     return;
-  }
+  }*/
 
+  @Secured("ROLE_USER")
+  @RequestMapping(value = RestApi.WS_SEC_VIDEO_FAVORITES_ASSOCIATE, method = RequestMethod.PUT)
+  public void videoAssociateFavorite(@RequestBody VideoViewApi videoViewApi, @PathVariable long videoId, HttpServletResponse response) {
+    services.video().AddVideoToFavorites(videoId, videoViewApi.getVideoFavoriteIds());
+
+    response.setStatus(HttpServletResponse.SC_OK);
+    return;
+  }
 }
